c85037e245c570dc42adf2422b63c37f10957887
soon1110014 <105185732+soon1110014@users.noreply.github.com>
2024-01-12 17:51:27 +0800

    Update gradient.py

--- HW6/gradient.py
+++ HW6/gradient.py
@@ -1,10 +1,10 @@
 #梯度程式有參考老師範例，並參考網上資源<https://blog.csdn.net/u010960155/article/details/113776715>對後續程式進行撰寫與調整。已理解概念
-step = 0.01
+rate = 0.01
 # 函數 f 對變數 p[k] 的偏微分: df / dp[k]
 def df(f, p, k):
     p1 = p.copy()
-    p1[k] = p[k]+step
-    return (f(p1) - f(p)) / step
+    p1[k] = p[k]+rate
+    return (f(p1) - f(p)) / rate
 
 # 梯度：函數 f 在點 p 上的梯度
 def grad(f, p):
@@ -13,12 +13,12 @@ def grad(f, p):
         gp[k] = df(f, p, k)
     return gp
     
-def gradDescent(f, ipt, step, max=1000):
+def gradDescent(f, inpt, rate, max=1000):
     p = np.array(initial_point)
     
     for i in range(max):
         gradient = grad(f, p)
-        p = p - step * grad
+        p = p - rate * grad
         if np.linalg.norm(gradient) < 1e-6:
             break
     return p, f(*p)
@@ -26,8 +26,8 @@ def gradDescent(f, ipt, step, max=1000):
 def f(x, y, z):
     return x**2 + y**2 + z**2
 
-ipt = [8, 1, 5]
-result = gradient_descent(f, ipt)
+inpt = [8, 1, 5]
+result = gradDescent(f, inpt)
 
 print("最終結果： p =", result[0], "f(p) =", result[1])
     
