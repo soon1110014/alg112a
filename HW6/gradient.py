f6e0a2407d144ef0d71e5393a2358b37749d7090
soon1110014 <105185732+soon1110014@users.noreply.github.com>
2024-01-12 17:09:34 +0800

    Update gradient.py

--- HW6/gradient.py
+++ HW6/gradient.py
@@ -1,1 +1,14 @@
 
+# 函數 f 對變數 p[k] 的偏微分: df / dp[k]
+def df(f, p, k):
+    p1 = p.copy()
+    p1[k] = p[k]+step
+    return (f(p1) - f(p)) / step
+
+# 梯度：函數 f 在點 p 上的梯度
+def grad(f, p):
+    gp = p.copy()
+    for k in range(len(p)):
+        gp[k] = df(f, p, k)
+    return gp
+  
