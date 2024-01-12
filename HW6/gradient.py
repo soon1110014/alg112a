ecdc0e717deeb0ed197fae2efe90ef695db0d8fc
soon1110014 <105185732+soon1110014@users.noreply.github.com>
2024-01-12 22:20:06 +0800

    Update gradient.py

--- HW6/gradient.py
+++ HW6/gradient.py
@@ -1,37 +1,32 @@
-#梯度程式有參考老師範例，並參考網上資源<https://blog.csdn.net/u010960155/article/details/113776715>對後續程式進行撰寫與調整，並透過ChatGPT進行除錯與改善。已理解概念
+#程式參考網站 https://blog.csdn.net/u010960155/article/details/113776715 ，有稍作修改並已理解程式邏輯與梯度下降概念
+
 import numpy as np
-rate = 0.01
-# 函數 f 對變數 p[k] 的偏微分: df / dp[k]
-def df(f, p, k):
-    p1 = p.copy()
-    p1[k] = p[k]+rate
-    return (f(*p1) - f(*p)) / rate
+import time
+ 
+def cal_grad(A, b, x):
+    left = np.dot(np.dot(A.T, A), x)
+    right = np.dot(A.T, b)
+    gradient = left - right
+    return gradient
+ 
+# iteration
+def gradDecent(x, A, b, learning_rate, step):
+    for i in range(step):
+        gradient = cal_grad(A, b, x)
+        delta = learning_rate * gradient
+        x = x - delta
+    print('近似解x = {a}'.format(a=x))
 
-# 梯度：函數 f 在點 p 上的梯度
-def grad(f, p):
-    gp = p.copy()
-    for k in range(len(p)):
-        gp[k] = df(f, p, k)
-    return gp
-    
-def gradDescent(f, inpt, max=1000):
-    p = np.array(inpt)
-    for i in range(max):
-        gradient = grad(f, p)
-        p = p - rate * gradient
-        if np.linalg.norm(gradient) < 0.00001:
-            break
-    return p, f(*p)
-
-def f(x, y, z):
-    return x**2 + y**2 + z**2
-
-inpt = [8, 1, 5]
-result = gradDescent(f, inpt)
-
-print("最終結果： p =", result[0], "f(p) =", result[1])
-    
+A = np.array([[1.0, -2.0, 1.0], [0.0, 2.0, -8.0], [-4.0, 5.0, 9.0]])
+b = np.array([0.0, 8.0, -9.0])
+# Giveb A and b，the solution x is [29, 16, 3]
+ 
+x0 = np.array([1.0, 1.0, 1.0])
+learning_rate = 0.01
+step = 1000000
+ 
+gradDecent(x0, A, b, learning_rate, step)
 #測試結果
 '''
-最終結果： p = [8. 1. 5.] f(p) = 90.0
+近似解x = [28.98272933 15.99042465  2.99763054]
 '''
